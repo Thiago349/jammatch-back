702e315dde95333e1a77330ae227e94a9ca75d2c
Thiago349 <thiagoreis2002@gmail.com>
2024-09-26 08:48:59 -0300

    feat: refactoring and adding auth system

---
+++ modules/spotify_services/controller.py
@@ -0,0 +1,56 @@
+from services.spotify.endpoints.client import SpotifyClient
+
+from modules.auth.service import AuthService
+
+from flask_restx import Namespace, Resource
+from flask import request
+
+api = Namespace(
+    "Spotify Services", path="/api/v1/spotify-services"
+)
+
+
+@api.route("/self")
+class RolesController(Resource):
+    def get(self):
+        try:
+            userInformation = AuthService.validate(request.headers)
+            if userInformation == None: 
+                return {"message": f"Unauthorized"}, 401
+
+            requestArgs = request.args
+            if 'userToken' not in requestArgs.keys():
+                return {"message": f"Bad Request: 'userToken' required"}, 400
+            
+            TOKEN = requestArgs['userToken']
+            if TOKEN is None:
+                return {"message": f"Unauthorized"}, 401
+            
+            headers = {
+                "Authorization": f"Bearer {TOKEN}"
+            }
+
+            return SpotifyClient.getSelf(headers), 200
+        except Exception as e:
+            api.logger.error("Error: %s", str(e))
+            return {"message": f"Internal Server Error: {str(e)}"}, 500
+
+
+@api.route("/auth")
+class RolesController(Resource):
+    def post(self):
+        try:
+            userInformation = AuthService.validate(request.headers)
+            if userInformation == None: 
+                return {"message": f"Unauthorized"}, 401
+
+            requestBody = request.json
+            if 'code' not in requestBody.keys():
+                return {"message": f"Bad Request: 'code' required"}, 400
+            
+            CODE = requestBody['code']
+
+            return SpotifyClient.authenticate(CODE), 201
+        except Exception as e:
+            api.logger.error("Error: %s", str(e))
+            return {"message": f"Internal Server Error: {str(e)}"}, 500
