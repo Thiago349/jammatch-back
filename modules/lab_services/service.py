9f6dec6d735de1b0870400446ede16d6ac1a29de
Thiago349 <thiagoreis2002@gmail.com>
2024-10-20 23:30:11 -0300

    add: adding genres and removing some params

--- modules/lab_services/service.py
+++ modules/lab_services/service.py
@@ -5,13 +5,11 @@ from modules.lab_services.mapper import LabServicesMapper
 class LabServicesService:
     def generateRamdomPlaylist(spotifyToken):
         randomParams = { 
-            "seed_genres": ",".join(random.sample(["pop", "rock", "jazz", "classical", "hip-hop", "country", "blues", "reggae", "electronic", "folk", "soul", "metal"], 3)),
+            "seed_genres": ",".join(random.sample(["pop", "rock", "jazz", "classical", "hip-hop", "country", "blues", "reggae", "electronic", "folk", "soul", "metal", "disco", "funk", "indie", "punk", "bossanova", "samba", "sertanejo", "forro", "salsa", "ska", "grunge", "mpb", "pagode"], 2)),
             "target_danceability": round(random.uniform(0.0, 1.0), 2),
             "target_energy": round(random.uniform(0.0, 1.0), 2),
             "target_acousticness": round(random.uniform(0.0, 1.0), 2),
             "target_instrumentalness": round(random.uniform(0.0, 1.0), 2),
-            "target_liveness": round(random.uniform(0.0, 1.0), 2),
-            "target_speechiness": round(random.uniform(0.0, 1.0), 2),
             "target_loudness": round(random.uniform(-60.0, 0.0), 2),
             "target_happiness": round(random.uniform(0.0, 1.0), 2)
         }
